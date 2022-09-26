07ee0f1822bae5c453a92f290c0970c15b87a051
Deepak <f12894343@gmail.com>
2022-09-26 11:07:51 +0530

    bugs removed

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {AuthScreen,OtpScreen,UserOnBoard} from './src/screens';
+
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [fontsLoaded] = useFonts({
+    'Poppins-SemiBold': require('./assets/fonts/Poppins-SemiBold.ttf'),
+    'Poppins-Regular': require('./assets/fonts/Poppins-Regular.ttf'),
+    'Poppins-Medium': require('./assets/fonts/Poppins-Medium.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading />
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        <AuthScreen/>
+        {/* <OtpScreen/> */}
+        {/* <UserOnBoard/> */}
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
---
+++ src/screens/index.js
@@ -0,0 +1,9 @@
+import AuthScreen from "./AuthenticationScreen/AuthScreen";
+import OtpScreen from "./AuthenticationScreen/OtpScreen";
+import UserOnBoard from "./AuthenticationScreen/UserOnBoard";
+
+export {
+    AuthScreen,
+    OtpScreen,
+    UserOnBoard,
+}
